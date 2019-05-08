398adf30c66688b0e369b809e2238630b8ce1422
olbender <olbender@users.noreply.github.com>
2019-05-08 05:47:03 +0200

    First working version, basic RTCP support.

--- src/opendlv-device-camera-rtp.cpp
+++ src/opendlv-device-camera-rtp.cpp
@@ -20,6 +20,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <random>
 
 #include <curl/curl.h>
 
@@ -28,9 +29,11 @@
 #include "sps-decoder.hpp"
 
 struct SdpData {
-  std::string streamUri;
-  std::string sps;
-  std::string pps;
+  std::map<uint32_t, std::string> encoding;
+  std::map<uint32_t, std::string> sps;
+  std::map<uint32_t, std::string> pps;
+  std::map<uint32_t, uint32_t> clockrate;
+  std::map<uint32_t, std::string> streamUri;
   double framerate;
 };
 
@@ -41,22 +44,42 @@ size_t parseSdpData(void *ptr, size_t size, size_t nmemb, void *userptr)
 
   std::istringstream f(static_cast<char *>(ptr));
   std::string line;
+  uint32_t latestPayloadType = 0;
   while (std::getline(f, line)) {
     {
-      std::string n("a=control:");
+      std::string n("a=rtpmap:");
       if (line.find(n) != -1) {
-        sdpData->streamUri = line.substr(n.length());
+        std::string s = line.substr(n.length());
+        uint32_t payloadType = std::stoi(s.substr(0, s.find(' ')));
+        latestPayloadType = payloadType;
+        std::string encoding = s.substr(s.find(' ') + 1,
+            s.find('/') - (s.find(' ') + 1));
+        uint32_t clockrate = std::stoi(s.substr(s.find('/') + 1));
+        sdpData->encoding[payloadType] = encoding;
+        sdpData->clockrate[payloadType] = clockrate;
       }
     }
     {
-      std::string n("sprop-parameter-sets=");
-      int32_t j = line.find(n);
-      if (j != -1) {
+      std::string n("a=fmtp:");
+      if (line.find(n) != -1) {
         cluon::FromJSONVisitor base64Decoder;
-        std::string spsAndPps = line.substr(j + n.length());
+
+        std::string s = line.substr(n.length());
+        uint32_t payloadType = std::stoi(s.substr(0, s.find(' ')));
+
+        n = "sprop-parameter-sets=";
+        std::string spsAndPps = s.substr(s.find(n) + n.length());
         int32_t i = spsAndPps.find(",");
-        sdpData->sps = base64Decoder.decodeBase64(spsAndPps.substr(0, i));
-        sdpData->pps = base64Decoder.decodeBase64(spsAndPps.substr(i + 1));
+        sdpData->sps[payloadType] = 
+          base64Decoder.decodeBase64(spsAndPps.substr(0, i));
+        sdpData->pps[payloadType] = 
+          base64Decoder.decodeBase64(spsAndPps.substr(i + 1));
+      }
+    }
+    {
+      std::string n("a=control:");
+      if (line.find(n) != -1) {
+        sdpData->streamUri[latestPayloadType] = line.substr(n.length());
       }
     }
     {
@@ -125,41 +148,90 @@ int32_t main(int32_t argc, char **argv)
       (0 == commandlineArguments.count("cid")) ) {
     std::cerr << argv[0] << " interfaces with the given RTSP/RTP-based camera "
       << "and sends the compressed frames as OpenDLV messages." << std::endl
-      << "Usage:   " << argv[0] << " --url=<URL> --cid=<CID> [--id<ID>] "
+      << "Usage:   " << argv[0] << " --url=<URL> --cid=<CID> "
+      << "[--server-port-udp-a=<Port>] [--id=<ID>] "
       << "[--verbose]" << std::endl
       << "         --cid:       CID of the OD4Session to receive Envelopes for "
       << "recording" << std::endl
-      << "         --id:        ID to use in case of multiple instances of" 
-      << argv[0] << std::endl
+      << "         --server-port-udp-a: The first UDP port to use (the second "
+      << "will always be the number after)"
+      << std::endl
+      << "         --id:        ID to use in case of multiple instances of "
+         "running microservices." 
+      << std::endl
       << "         --url:       URL providing an MJPEG stream over http" 
       << std::endl
       << "         --verbose:   display captured image" << std::endl
       << "Example: " << argv[0] << " --url=http://192.168.0.11?mjpeg "
       << "--verbose --cid=111" << std::endl;
   } else {
-    std::string const URL{commandlineArguments["url"]};
-    uint32_t ID{(commandlineArguments.count("id") != 0) ?
+    std::string const url{commandlineArguments["url"]};
+    uint32_t senderStamp{(commandlineArguments.count("id") != 0) ?
         static_cast<uint32_t>(std::stoi(commandlineArguments["id"])) : 0};
-    bool VERBOSE{commandlineArguments.count("verbose") != 0};
+    bool verbose{commandlineArguments.count("verbose") != 0};
 
-    std::string hostname = getHostname(URL);
+    uint32_t const serverPortA = {
+      (commandlineArguments.count("server-port-udp-a") != 0) ?
+        static_cast<uint32_t>(
+            std::stoi(commandlineArguments["server-port-udp-a"])) : 50000};
+    uint32_t const serverPortB = serverPortA + 1;
+
+    uint32_t clientPortA = 33000;
+    uint32_t clientPortB = 33001;
+    for (uint32_t port = 33000; port < 34000; port += 2) {
+      int32_t clientSocketA = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+      struct sockaddr_in src;
+      std::memset(&src, 0, sizeof(src));
+      src.sin_family = AF_INET;
+      src.sin_addr.s_addr = htonl(INADDR_ANY);
+      src.sin_port = htons(port); 
+
+      if (bind(clientSocketA, reinterpret_cast<struct sockaddr *>(&src), 
+            sizeof(src)) != 0) {
+        close(clientSocketA);
+        continue;
+      }
+      
+      int32_t clientSocketB = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+      
+      std::memset(&src, 0, sizeof(src));
+      src.sin_family = AF_INET;
+      src.sin_addr.s_addr = htonl(INADDR_ANY);
+      src.sin_port = htons(port + 1); 
+
+      if (bind(clientSocketB, reinterpret_cast<struct sockaddr *>(&src), 
+            sizeof(src)) != 0) {
+        close(clientSocketA);
+        close(clientSocketB);
+        continue;
+      }
+        
+      close(clientSocketA);
+      close(clientSocketB);
+
+      clientPortA = port;
+      clientPortB = port + 1;
+      break;
+    }
+
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    std::uniform_int_distribution<uint32_t> dis;
+
+    std::string hostname = getHostname(url);
     std::string localHostname;
 
-    // TODO: Find automatically.
-    uint32_t const clientPortA = 33056;
-    uint32_t const clientPortB = 33057;
-    uint32_t const serverPortA = 50000;
-    uint32_t const serverPortB = 50001;
+    uint32_t const clientSsrc = dis(gen);
 
     cluon::OD4Session od4{
       static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
 
     CURL *curl = curl_easy_init();
 
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, (VERBOSE ? 1 : 0));
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
     curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1);
-    curl_easy_setopt(curl, CURLOPT_HEADERDATA, stdout);
-    curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
    // curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
 
     std::string const transport("RTP/AVP;unicast;client_port=" 
@@ -167,7 +239,7 @@ int32_t main(int32_t argc, char **argv)
     std::string const range("npt=0.000-");
 
     // RTSP options
-    curl_easy_setopt(curl, CURLOPT_RTSP_STREAM_URI, URL.c_str());
+    curl_easy_setopt(curl, CURLOPT_RTSP_STREAM_URI, url.c_str());
     curl_easy_setopt(curl, CURLOPT_RTSP_REQUEST, CURL_RTSPREQ_OPTIONS);
     curl_easy_perform(curl);
 
@@ -187,26 +259,35 @@ int32_t main(int32_t argc, char **argv)
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, nullptr);
 
     // RTSP setup
-    curl_easy_setopt(curl, CURLOPT_RTSP_STREAM_URI, sdpData.streamUri.c_str());
-    curl_easy_setopt(curl, CURLOPT_RTSP_TRANSPORT, transport.c_str());
-    curl_easy_setopt(curl, CURLOPT_RTSP_REQUEST, CURL_RTSPREQ_SETUP);
-    curl_easy_perform(curl);
+    for (auto &streamUri : sdpData.streamUri) {
+      if (streamUri.first == 96) {
+        if (verbose) {
+          std::cout << "Found H.264 stream at " << streamUri.second 
+            << ". Setting up." << std::endl;
+        }
+        curl_easy_setopt(curl, CURLOPT_RTSP_STREAM_URI, streamUri.second.c_str());
+        curl_easy_setopt(curl, CURLOPT_RTSP_TRANSPORT, transport.c_str());
+        curl_easy_setopt(curl, CURLOPT_RTSP_REQUEST, CURL_RTSPREQ_SETUP);
+        curl_easy_perform(curl);
+      }
+    }
 
     // Send magic number
     sendMagicNumber(clientPortA, hostname, serverPortA);
     sendMagicNumber(clientPortB, hostname, serverPortB);
 
     // RTSP play
-    curl_easy_setopt(curl, CURLOPT_RTSP_STREAM_URI, URL.c_str());
+    curl_easy_setopt(curl, CURLOPT_RTSP_STREAM_URI, url.c_str());
     curl_easy_setopt(curl, CURLOPT_RANGE, range.c_str());
     curl_easy_setopt(curl, CURLOPT_RTSP_REQUEST, CURL_RTSPREQ_PLAY);
     curl_easy_perform(curl);
     curl_easy_setopt(curl, CURLOPT_RANGE, nullptr);
 
 
+    // TODO: make right (properly support different payload types)
     SpsInfo spsInfo = decodeSps(
-        reinterpret_cast<uint8_t const *>(sdpData.sps.data()),
-        sdpData.sps.length());
+        reinterpret_cast<uint8_t const *>(sdpData.sps[96].data()),
+        sdpData.sps[96].length());
     uint32_t width = spsInfo.width;
     uint32_t height = spsInfo.height;
 
@@ -216,8 +297,16 @@ int32_t main(int32_t argc, char **argv)
 
     std::string outData;
 
+    std::mutex rtcpMutex;
+    cluon::data::TimeStamp latestNtpTime;
+    uint64_t latestRtpTime;
+    double jitter = 700.0; // TODO: Calculate jitter (easy)
+    uint32_t highestSeq = 0;
+
+
     auto onStreamData =
-      [&od4, &outData, &width, &height, &sdpData, &ID, &VERBOSE](
+      [&od4, &outData, &width, &height, &sdpData, &rtcpMutex,
+      &latestNtpTime, &latestRtpTime, &highestSeq, &senderStamp, &verbose](
         std::string &&data, std::string &&,
         std::chrono::system_clock::time_point &&) noexcept {
       char const *buf_start = static_cast<char const *>(data.c_str());
@@ -225,14 +314,19 @@ int32_t main(int32_t argc, char **argv)
       static uint8_t const nalPrefix[] = {0x00, 0x00, 0x01};
 
       uint8_t const b0 = *buf_start;
-      uint8_t const version = (b0 >> 6);
+     // uint8_t const version = (b0 >> 6);
       bool const hasPadding = (b0 & 0x20) >> 5;
-      bool const hasExtension = (b0 & 0x10) >> 4;
-      uint8_t const csrcCount = (b0 & 0xf);
+     // bool const hasExtension = (b0 & 0x10) >> 4;
+     // uint8_t const csrcCount = (b0 & 0xf);
       
       uint8_t const b1 = *(buf_start + 1);
-      bool const profileMarker = (b1 >> 7);
-      uint8_t const profileType = (b1 & 0x7f);
+     // bool const profileMarker = (b1 >> 7);
+      uint8_t const payloadType = (b1 & 0x7f);
+
+      if (payloadType != 96) {
+        std::cout << "WARNING: Unknown format " << +payloadType << std::endl;
+        return;
+      }
 
       uint32_t b2b3;
       memcpy(&b2b3, buf_start + 2, 2);
@@ -261,17 +355,26 @@ int32_t main(int32_t argc, char **argv)
       uint8_t const h264RtpType = (b12 & 0x1f);
       uint8_t nalType;
 
+      cluon::data::TimeStamp ts;
+      {
+        std::lock_guard<std::mutex> lock(rtcpMutex);
+        uint64_t rtpTimeInMicroseconds = (timestamp - latestRtpTime)
+          * (1000000UL / sdpData.clockrate[payloadType]);
+        ts = cluon::time::fromMicroseconds(
+            cluon::time::toMicroseconds(latestNtpTime)
+            + rtpTimeInMicroseconds);
+        highestSeq = sequenceNumber > highestSeq ? sequenceNumber : highestSeq;
+      }
+
       if (h264RtpType >= 1 && h264RtpType <= 23) {
-        std::cout << "hej" << std::endl;
         nalType = h264RtpType;
         uint32_t nalLen = data.length() - 12 - paddingLen;
         outData = std::string(&nalPrefix[0], &nalPrefix[0] + 3) 
             + std::string(buf_start + 12, nalLen);
 
-
         opendlv::proxy::ImageReading ir;
         ir.fourcc("h264").width(width).height(height).data(outData);
-        od4.send(ir, cluon::time::now(), ID);
+        od4.send(ir, ts, senderStamp);
 
         outData = "";
 
@@ -284,8 +387,10 @@ int32_t main(int32_t argc, char **argv)
         std::string extra;
         if (isStartFragment) {
           uint8_t nalHeader = (h264RtpNri << 5) | nalType;
-          extra = std::string(&nalPrefix[0], &nalPrefix[0] + 3) + sdpData.sps 
-            + std::string(&nalPrefix[0], &nalPrefix[0] + 3) + sdpData.pps  
+          extra = std::string(&nalPrefix[0], &nalPrefix[0] + 3) 
+            + sdpData.sps[payloadType] 
+            + std::string(&nalPrefix[0], &nalPrefix[0] + 3) 
+            + sdpData.pps[payloadType]
             + std::string(&nalPrefix[0], &nalPrefix[0] + 3)
             + static_cast<char>(nalHeader);
         }
@@ -296,7 +401,7 @@ int32_t main(int32_t argc, char **argv)
         if (isEndFragment) {
           opendlv::proxy::ImageReading ir;
           ir.fourcc("h264").width(width).height(height).data(outData);
-          od4.send(ir, cluon::time::now(), ID);
+          od4.send(ir, ts, senderStamp);
 
           outData = "";
         }
@@ -304,16 +409,147 @@ int32_t main(int32_t argc, char **argv)
         std::cout << "WARNING: unknown RTP H264 payload type: " << h264RtpType
           << std::endl;
       }
-
-      if (VERBOSE) {
-        std::cout << "Timestamp: " << +timestamp << std::endl;
-      }
     };
     
-    auto onControlData = [&VERBOSE](std::string &&data, std::string &&,
-        std::chrono::system_clock::time_point &&) noexcept {
-      if (VERBOSE) {
-        std::cout << "Got control data: " << std::endl;
+    auto onControlData = [&clientPortB, &serverPortB, &clientSsrc, &hostname, 
+         &rtcpMutex, &latestNtpTime, &latestRtpTime, &jitter, &highestSeq,
+         &verbose](
+             std::string &&data, std::string &&, 
+             std::chrono::system_clock::time_point &&dataInTs) noexcept {
+
+      char const *buf_start = static_cast<char const *>(data.c_str());
+
+    //  uint8_t const b0 = *buf_start;
+    //  uint8_t const version = (b0 >> 6);
+    //  bool const hasPadding = (b0 & 0x20) >> 5;
+    //  uint8_t const receptionReportCount = (b0 & 0x1f);
+
+      uint8_t const b1 = *(buf_start + 1);
+      uint8_t const packetType = b1;
+      
+    //  uint16_t b2b3;
+    //  memcpy(&b2b3, buf_start + 2, 2);
+    //  uint8_t const length = ntohs(b2b3);
+
+      uint32_t b4b5b6b7;
+      memcpy(&b4b5b6b7, buf_start + 4, 4);
+      uint32_t const ssrcId = ntohl(b4b5b6b7);
+
+      if (packetType == 200) {
+
+        uint32_t b8b9b10b11;
+        memcpy(&b8b9b10b11, buf_start + 8, 4);
+        uint32_t const ntpMsw = ntohl(b8b9b10b11);
+      
+        uint32_t b12b13b14b15;
+        memcpy(&b12b13b14b15, buf_start + 12, 4);
+        uint32_t const ntpLsw = ntohl(b12b13b14b15);
+        
+        uint32_t b10b11b12b13;
+        memcpy(&b10b11b12b13, buf_start + 10, 4);
+      
+        uint64_t const sec = ntpMsw - 2208988800ULL;
+        uint64_t const usec = (ntpLsw * 1000000UL) >> 32;
+
+        cluon::data::TimeStamp ntpTime = cluon::time::fromMicroseconds(
+            sec * 1000000UL + usec);
+        
+        uint32_t b16b17b18b19;
+        memcpy(&b16b17b18b19, buf_start + 16, 4);
+        uint32_t const rtpTime = ntohl(b16b17b18b19);
+
+        uint32_t jitter_n;
+        uint16_t highestSeq_n;
+        {
+          std::lock_guard<std::mutex> lock(rtcpMutex);
+          latestNtpTime = ntpTime;
+          latestRtpTime = rtpTime;
+          jitter_n = htonl(static_cast<uint32_t>(jitter));
+          highestSeq_n = htons(highestSeq);
+        }
+
+
+        // Send Receiver report.
+        {
+          int32_t fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+          struct sockaddr_in src;
+          std::memset(&src, 0, sizeof(src));
+          src.sin_family = AF_INET;
+          src.sin_addr.s_addr = htonl(INADDR_ANY);
+          src.sin_port = htons(clientPortB); 
+
+          bind(fd, reinterpret_cast<struct sockaddr *>(&src), sizeof(src));
+          
+          struct sockaddr_in dst;
+          std::memset(&dst, 0, sizeof(dst));
+          dst.sin_family = AF_INET;
+          dst.sin_addr.s_addr = inet_addr(hostname.c_str());
+          dst.sin_port = htons(serverPortB);
+
+          uint8_t data[48] = {};
+          {
+            data[0] = 0x81;
+            data[1] = 0xc9;
+
+            uint16_t length{7};
+            uint16_t length_n = htons(length);
+            memcpy(data + 2, &length_n, 2);
+
+            uint32_t clientSsrc_n = htonl(clientSsrc);
+            memcpy(data + 4, &clientSsrc_n, 4);
+            
+            memcpy(data + 8, &b4b5b6b7, 4);
+
+            uint32_t fractionAndCumulativeLost{0};
+            uint32_t fractionAndCumulativeLost_n 
+              = htonl(fractionAndCumulativeLost);
+            memcpy(data + 12, &fractionAndCumulativeLost_n, 4);
+            
+            uint16_t extendedHighestSeq{1};
+            uint16_t extendedHighestSeq_n = htons(extendedHighestSeq);
+            memcpy(data + 16, &extendedHighestSeq_n, 2);
+            memcpy(data + 18, &highestSeq_n, 2);
+            
+            memcpy(data + 20, &jitter_n, 4);
+            
+            memcpy(data + 24, &b10b11b12b13, 4);
+
+            double timeSinceSr = std::chrono::duration<double>(
+                  std::chrono::high_resolution_clock::now() - dataInTs).count();
+            uint32_t delaySinceLastSr = static_cast<uint32_t>(
+                timeSinceSr / 65536);
+            uint32_t delaySinceLastSr_n = htonl(delaySinceLastSr);
+            memcpy(data + 28, &delaySinceLastSr_n, 4);
+          }
+
+          {
+            data[32] = 0x81;
+            data[33] = 0xca;
+
+            uint16_t length{3};
+            uint16_t length_n = htons(length);
+            memcpy(data + 34, &length_n, 2);
+            
+            uint32_t clientSsrc_n = htonl(clientSsrc);
+            memcpy(data + 36, &clientSsrc_n, 4);
+            
+            data[40] = 0x01;
+            data[41] = 0x05;
+            data[42] = 0x73;
+            data[43] = 0x68;
+            data[44] = 0x72;
+            data[45] = 0x65;
+            data[46] = 0x77;
+            data[47] = 0x00;
+          }
+
+          sendto(fd, data, 48, 0, 
+              reinterpret_cast<const struct sockaddr *>(&dst), sizeof(dst));
+
+          shutdown(fd, SHUT_RDWR);
+          close(fd);
+        }
       }
     };
 
@@ -324,8 +560,18 @@ int32_t main(int32_t argc, char **argv)
       cluon::UDPReceiver controlUdpReceiver{localHostname, clientPortB,
         onControlData, serverPortB};
 
+      uint32_t const heartbeatInterval = 50;
+      uint32_t h = 0;
       while (od4.isRunning()) {
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+        if (h > heartbeatInterval) {
+          curl_easy_setopt(curl, CURLOPT_RTSP_REQUEST, CURL_RTSPREQ_OPTIONS);
+          curl_easy_perform(curl);
+          h = 0;
+        } else {
+          h++;
+        }
       }
     }
 
